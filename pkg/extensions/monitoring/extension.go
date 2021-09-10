428a58fffe17bf46b75ec079c059cb18722be76a
Alexei Dodon <adodon@cisco.com>
2021-09-10 16:44:35 +0300

    removed pkg/extensions/prometheus/metrics package

--- pkg/extensions/monitoring/extension.go
+++ pkg/extensions/monitoring/extension.go
@@ -3,32 +3,77 @@
 package monitoring
 
 import (
-	"time"
-	"regexp"
-	"strconv"
 	"os"
 	"path"
 	"path/filepath"
-	
-	"github.com/anuvu/zot/pkg/extensions/prometheus/metrics"
+	"regexp"
+	"strconv"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var metricsNamespace = "zot"
+var (
+	HttpConnRequests = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: metricsNamespace,
+			Name:      "http_requests_total",
+			Help:      "Total number of http request in zot",
+		},
+		[]string{"method", "code"},
+	)
+	HttpServeLatency = promauto.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Namespace: metricsNamespace,
+			Name:      "http_latency_seconds",
+			Help:      "Latency of serving HTTP requests",
+		},
+		[]string{"repo"},
+	)
+	StorageUsage = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: metricsNamespace,
+			Name:      "storage_usage_bytes",
+			Help:      "Storage used",
+		},
+		[]string{"repo"},
+	)
+	UploadCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: metricsNamespace,
+			Name:      "upload_image_total",
+			Help:      "Total number times an image was uploaded",
+		},
+		[]string{"repo"},
+	)
+	DownloadCounter = promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Namespace: metricsNamespace,
+			Name:      "download_image_total",
+			Help:      "Total number times an image was downloaded",
+		},
+		[]string{"repo"},
+	)
 )
 
 func IncHttpConnRequests(method string, statusCode int) {
-	metrics.HttpConnRequests.WithLabelValues(method, strconv.Itoa(statusCode)).Inc()
+	HttpConnRequests.WithLabelValues(method, strconv.Itoa(statusCode)).Inc()
 }
 
 func ObserveHttpServeLatency(path string, latency time.Duration) {
 	re := regexp.MustCompile("\\/v2\\/(.*?)\\/(blobs|tags|manifests)\\/(.*)$")
 	match := re.FindStringSubmatch(path)
 	if len(match) > 1 {
-		metrics.HttpServeLatency.WithLabelValues(match[1]).Observe(latency.Seconds())
+		HttpServeLatency.WithLabelValues(match[1]).Observe(latency.Seconds())
 	} else {
-		metrics.HttpServeLatency.WithLabelValues("N/A").Observe(latency.Seconds())
+		HttpServeLatency.WithLabelValues("N/A").Observe(latency.Seconds())
 	}
 }
 
 func IncDownloadCounter(repo string) {
-	metrics.DownloadCounter.WithLabelValues(repo).Inc()
+	DownloadCounter.WithLabelValues(repo).Inc()
 }
 
 func SetStorageUsage(repo string, rootDir string) {
@@ -36,12 +81,12 @@ func SetStorageUsage(repo string, rootDir string) {
 	repoSize, err := getDirSize(dir)
 
 	if err == nil {
-		metrics.StorageUsage.WithLabelValues(repo).Set(float64(repoSize))
+		StorageUsage.WithLabelValues(repo).Set(float64(repoSize))
 	}
 }
 
 func IncUploadCounter(repo string) {
-	metrics.UploadCounter.WithLabelValues(repo).Inc()
+	UploadCounter.WithLabelValues(repo).Inc()
 }
 
 func getDirSize(path string) (int64, error) {
